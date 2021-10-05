085bfcbbbfe14f20ab111c60a51a99da7f44fa74
Bobka9998 <sultanovboburbek9998@gmail.com>
2021-10-05 17:15:15 +0500

    new code

---
+++ dars4.py
@@ -0,0 +1,13 @@
+# vazifa 1
+# kocha= "Bog`bon"
+# mahalla= "Sag`bon"
+# tuman= "bobdomzor"
+# viloyat= "samarqand"
+# print(kocha + " ko`chasi,", mahalla + " mahallasi,",  tuman.title() + " tumani,", viloyat.title() + " viloyati.")
+#vazifa 2
+kocha= input("Ko`changiz?\n")
+mahalla= input("Mahallangiz?\n")
+tuman= input("Tumaningiz?\n")
+viloyat= input("Viloyatingiz?\n")
+print(f"{kocha.capitalize()} ko`chasi" + ' ' + f"{mahalla.capitalize()} mahallasi" + ' ' + f"{tuman.capitalize()} tumani" + ' ' + 
+f"{viloyat.capitalize()} viloyati")
